96bf4d1258c3e33b417969659d99f7bb745d98dd
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-24 08:54:18 +0000

    commit

---
+++ networking/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_cidrs" {
+  type = list(any)
+}
+
+variable "public_sn_count" {
+  type = number
+}
+
+variable "map_public_ip_on_launch" {
+  type = string
+}
+
+
+# variable "access_ip" {
+#   type = string
+# }
